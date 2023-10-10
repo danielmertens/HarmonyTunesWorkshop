33023e390bed1d08f07d4233333c326f99e1b300
Daniel Mertens <daniel.mertens@infosupport.com>
2023-10-11 01:32:59 +0200

    day 2 part 2

---
+++ Day 2/Part 2/HarmonyTunes.Catalogue/Song/Application/Interfaces/ISongQueryService.cs
@@ -0,0 +1,9 @@
+﻿using HarmonyTunes.Catalogue.Song.Application.Models;
+
+namespace HarmonyTunes.Catalogue.Song.Application.Interfaces;
+
+public interface ISongQueryService
+{
+    Task<IEnumerable<SongQueryModel>> GetAll();
+    Task<SongQueryModel?> GetById(Guid id);
+}
